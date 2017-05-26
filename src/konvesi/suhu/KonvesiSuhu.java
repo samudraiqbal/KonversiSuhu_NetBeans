f993e104ce4f3758c76568f1a7028d6243b108c8
M Iqbal Samudra <11samudra00@gmail.com>
2017-05-26 16:32:44 +0700

    Done

---
+++ src/konvesi/suhu/KonvesiSuhu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konvesi.suhu;
+
+/**
+ *
+ * @author Samudra~
+ */
+public class KonvesiSuhu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
